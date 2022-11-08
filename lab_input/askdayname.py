fb261f2793c27cad73700a61b7f68bfe5ec07823
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-11-08 20:33:38 +0000

    join and split

---
+++ lab_input/askdayname.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def ask():
+
+    name = input("Hello! What's your name?")
+
+    day = input("What day is it today?")
+
+    print("Hello, " + name + "! Happy " + day + "!")
+ask()
