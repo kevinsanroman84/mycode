e351d7e86ead3559a87f2713f09b699bd6882707
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-11-15 21:46:07 +0000

    Pulling data from API with  python standard library

--- mini_rpg/character.py
+++ mini_rpg/character.py
@@ -1,17 +1,37 @@
+
+
+# Classes for players, monsters, and npcs
 class Character():
-    def __init__(self, name):
+    def __init__(self, name, health=1, attack=1, defense=1, speed=1):
         self.name = name
-        self.health = 1
-        self.attack = 1 
-        self.defense = 1
-        self.speed = 1
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
     
     # Checks targets defense and your attack for damage.
     def attack(self, target):
         return
-    
-class Player(Character):
-    def __init__(self, name, job):
-        self.job = job
 
-    Person.__init__(self, name)
+# Specific attributes and classes for the player character
+class Player(Character):
+    def __init__(self, name, health, attack, defense, speed):
+        Character.__init__(self, name)
+        self.moves = 0
+
+# Job classes for specific stats for players
+class Rogue(Player):
+    def __init__(self, name, health=1, attack=1, defense=1, speed=1):
+        Player.__init__(self, name, health, attack, defense, speed)
+
+class Barbarian(Player):
+    def __init__(self, name, health, attack, defense, speed):
+        Player.__init__(name, health, attack, defense, speed)
+
+class Wizard(Player):
+    def __init__(self, name, health, attack, defense, speed):
+        Player.__init__(name, health, attack, defense, speed)
+
+class Paladin(Player):
+    def __init__(self, name, health, attack, defense, speed):
+        Player.__init__(name, health, attack, defense, speed)
