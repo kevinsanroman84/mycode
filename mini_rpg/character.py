21953b02e9aa28a053189b0a36fb07a289f58226
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-11-15 16:32:39 +0000

    Learning about sub classes

---
+++ mini_rpg/character.py
@@ -0,0 +1,15 @@
+class Character():
+    def __init__(self):
+        self.name = ""
+        self.health = 1
+        self.attack = 1 
+        self.defense = 1
+        self.speed = 1
+    
+    # Checks targets defense and your attack for damage.
+    def attack(self, target):
+        return
+    
+class Player(Character):
+    def __init__(self):
+        
